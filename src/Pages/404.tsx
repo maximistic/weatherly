159a16748879c195c9439096790471f80c087dbd
maximistic <110153672+maximistic@users.noreply.github.com>
2024-12-10 11:17:44 +0530

    splits

---
+++ src/Pages/404.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+      <div className="p-8">
+        <h1 className="text-2xl font-bold text-red-500">404 - Page Not Found</h1>
+        <p>The page you&apos;re looking for doesn&apos;t exist.</p>
+      </div>
+    );
+  }  
